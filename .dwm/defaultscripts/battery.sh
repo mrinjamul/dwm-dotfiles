a414c689f3128c2ce7c9c6f504ea4a40abf60d45
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-05-29 10:53:09 +0530

    Update scripts & cheatsheet added

--- .dwm/defaultscripts/battery.sh
+++ .dwm/defaultscripts/battery.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+#acpi -b
+
 ICON=""
 read -r capacity </sys/class/power_supply/BAT0/capacity
 read -r state </sys/class/power_supply/BAT0/status
 #printf "$ICON%s%%" "$capacity"
 
-echo "$ICON $capacity% $state"
-#acpi -b
+
+if [ "$state" == "Charging" ]; then
+echo "$ICON $capacity% ↯"
+elif [ "$state" == "Discharging" ]; then
+echo "$ICON $capacity% ⍗"
+else
+echo "$ICON $capacity% ⎊"
+fi
